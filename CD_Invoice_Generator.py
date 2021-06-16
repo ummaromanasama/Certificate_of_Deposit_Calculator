ba1e0f7351c726d05dc92926de213483e0e56b94
Ummaromana Sama <ummaromanasama@gmail.com>
2021-06-15 21:29:23 -0400

    Add files via upload

--- CD_Invoice_Generator.py
+++ CD_Invoice_Generator.py
@@ -1,13 +1,8 @@
 from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.platypus.tables import Table
 
 #----------------------------------CD Information--------------------------------------
-#Users information
-# name = input('Name: ')
-# email = input('Email: ')
-# address = input('Address: ')
-# bank_name = input('Bank name: ')
-
-
 # # Input the deposit amount
 # year = int(input('Enter the intial year of the CD account: '))
 
@@ -20,22 +15,23 @@ from reportlab.pdfgen import canvas
 
 # # Input number of years account will earn interest.
 # time = int(input('Enter how many years the account will accrue interest: '))
-
 # year += time
 
 # # The compund rate is monthly.
 # compound = 12
 
-# # Calculate the total amount
+# # # Calculate the total amount
 # total_amount = (amount * (1 + annual_interest_rate/ compound) ** (compound * time))
 # total_intrest = total_amount - amount
 
-# Display the total amount.
+# # Display the total amount.
 # print('At the end of', year,'you will have $',format(total_amount, '.2f'))
 # print('The total amount of interest accumulated is $',format(total_intrest, '.2f'))
 
-pdf = canvas.Canvas("CD_Invoice_2020.pdf")
+# Generate invoice
+pdf = canvas.Canvas("CD_Invoice_2021.pdf")
 pdf.setTitle('Certificate Deposit')
+pdf.setPageSize((700, 500))
 
 def drawMyRuler(pdf):
     pdf.drawString(100,810, 'x100')
@@ -55,8 +51,16 @@ def drawMyRuler(pdf):
 
 drawMyRuler(pdf)
 
-pdf.drawString(100, 800, 'bank_name')
-pdf.drawString(100, 750, 'name')
-pdf.drawString(100, 740, 'email')
-pdf.drawString(100, 730, 'address')
+pdf.setFont("Helvetica", 12)
+pdf.drawString(100, 450, 'Jane Doe')
+pdf.drawString(100, 438, 'jane.doe@gmail.com')
+pdf.drawString(100, 425, '121-11 103 Ave, Maple street, NY 12124')
+
+pdf.drawString(400, 450, 'Capital One')
+pdf.drawString(400, 438, 'Capital.One@gmail.com')
+pdf.drawString(400, 425, '341-01 45 Ave, Sky street, NJ 16933')
+
+# pdf.translate(600, 690)
+pdf.rect(.5*inch,5.8*inch,8.8*inch,.005*inch, fill=1)
+
 pdf.save()
